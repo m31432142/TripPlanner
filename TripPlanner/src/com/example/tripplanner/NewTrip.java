d34c5e4241e3bc4e5359f6748eb18bd0ff858d55
Patrick <leger_patr@bentley.edu>
2015-03-28 09:30:43 -0400

    new_trip.xml: Added buttons to user interface NewTrip.java: Added logic click logic for the buttons. strings.xml: Added string variables for the new buttons on new_trip.xml

--- TripPlanner/src/com/example/tripplanner/NewTrip.java
+++ TripPlanner/src/com/example/tripplanner/NewTrip.java
@@ -15,16 +15,75 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class NewTrip extends Activity{
+public class NewTrip extends Activity implements OnClickListener{
 
+	//Declare variables for widgets
+	private EditText tripName;
+	private EditText destination;
+	private EditText startDate;
+	private EditText endDate;
+	private Button btnCancel;
+	private Button btnSave;
+	private Button btnPackingList;
+	private Button btnToDoList;
+	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.new_trip);			
+		setContentView(R.layout.new_trip);		
 		
-	
+		//Link Java variables with xml widgets
+		//Set OnClickListener on Buttons
+		tripName = (EditText) findViewById(R.id.inputName);
+		destination = (EditText) findViewById(R.id.inputDestination);
+		startDate = (EditText) findViewById(R.id.startDate);
+		endDate = (EditText) findViewById(R.id.endDate);
+		btnCancel = (Button) findViewById(R.id.btnCancelNT);
+		btnCancel.setOnClickListener(this);
+		btnSave = (Button) findViewById(R.id.btnSaveNT);
+		btnSave.setOnClickListener(this);
+		btnPackingList = (Button) findViewById(R.id.btnPackingListNT);
+		btnPackingList.setOnClickListener(this);
+		btnToDoList = (Button) findViewById(R.id.btnToDoListNT);
+		btnToDoList.setOnClickListener(this);	
    }
+	
+	public void onClick(View v){
+		//Currently test code to make sure buttons are working. 
+		switch(v.getId()){
+		
+		//If Cancel Button is clicked. 
+		case R.id.btnCancelNT:
+			Toast.makeText(this,"You clicked the " + 
+					btnCancel.getText().toString()+ " Button.",Toast.LENGTH_SHORT)
+			.show();
+			break;
+		
+		//If Save Button is clicked. 
+		case R.id.btnSaveNT:
+			Toast.makeText(this,"You clicked the " + 
+					btnSave.getText().toString()+ " Button.",Toast.LENGTH_SHORT)
+			.show();
+			break;
+		
+		//If Packing List Button is clicked. 
+		case R.id.btnPackingListNT:
+			Toast.makeText(this,"You clicked the " + 
+					btnPackingList.getText().toString()+ " Button.",Toast.LENGTH_SHORT)
+			.show();
+			break;
+		
+		//If To Do List Button is clicked. 
+		case R.id.btnToDoListNT:
+			Toast.makeText(this,"You clicked the " + 
+					btnToDoList.getText().toString()+ " Button.",Toast.LENGTH_SHORT)
+			.show();
+			break;		
+		}
+		
+	}
 }
